5dda592727a3ba279ec1477ed7ed9ecde531d9a0
naruto_chen <1187340828@qq.com>
2024-11-03 17:35:05 +0800

    FIX000001#项目通用方法初始化

--- src/main/java/com/local/naruto/common/config/MybatisPlusConfig.java
+++ src/main/java/com/local/naruto/common/config/MybatisPlusConfig.java
@@ -2,7 +2,9 @@ package com.local.naruto.common.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,23 +25,23 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         // 分页插件
-//        interceptor.addInnerInterceptor(paginationInnerInterceptor());
+        interceptor.addInnerInterceptor(paginationInnerInterceptor());
         // 乐观锁插件
         interceptor.addInnerInterceptor(optimisticLockerInnerInterceptor());
         // 阻断插件
-//        interceptor.addInnerInterceptor(blockAttackInnerInterceptor());
+        interceptor.addInnerInterceptor(blockAttackInnerInterceptor());
         return interceptor;
     }
 
     /**
      * 分页插件，自动识别数据库类型 <a href="https://baomidou.com/guide/interceptor-pagination.html">...</a>
      */
-//    private PaginationInnerInterceptor paginationInnerInterceptor() {
-//        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
-//        // 设置最大单页限制数量，默认 500 条，-1 不受限制
-//        paginationInnerInterceptor.setMaxLimit(500L);
-//        return paginationInnerInterceptor;
-//    }
+    private PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(500L);
+        return paginationInnerInterceptor;
+    }
 
     /**
      * 乐观锁插件 <a href="https://baomidou.com/guide/interceptor-optimistic-locker.html">...</a>
@@ -60,8 +62,8 @@ public class MybatisPlusConfig {
      * 如果是对全表的删除或更新操作，就会终止该操作 <a
      * href="https://baomidou.com/guide/interceptor-block-attack.html">...</a>
      */
-//    private BlockAttackInnerInterceptor blockAttackInnerInterceptor() {
-//        return new BlockAttackInnerInterceptor();
-//    }
+    private BlockAttackInnerInterceptor blockAttackInnerInterceptor() {
+        return new BlockAttackInnerInterceptor();
+    }
 }
 
